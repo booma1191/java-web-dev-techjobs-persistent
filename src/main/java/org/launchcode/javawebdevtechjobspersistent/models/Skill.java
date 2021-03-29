803f2150cde3a928c7b978dd73903b4599fa84db
booma1191 <booma1191.m@gmail.com>
2021-03-29 06:02:21 -0500

    completed step 2. everything works except clicking on a skill to see the description. Will fix later.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank
+    private String description;
+
+    public Skill() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
